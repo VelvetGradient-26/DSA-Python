a4d59d6aa26da03509341dfa46e8a855abcbf345
Deepak <www.deepakkrishna9845@gmail.com>
2026-06-13 10:42:52 +0530

    Refactor product of array except self methods for consistency and readability

--- LC-Questions/238-product-of-array-except-self.py
+++ LC-Questions/238-product-of-array-except-self.py
@@ -1,30 +1,34 @@
 from typing import List
 
 class Solution:
-    def bruteProductExceptSelf(self, nums: List[int]) -> List[int]:
+    """Solution for LeetCode 238: Product of Array Except Self."""
+
+    def brute_product_except_self(self, nums: List[int]) -> List[int]:
+        """Brute-force product of array except self (O(n^2))."""
         n = len(nums)
         products = []
-        for i in range(n): 
+        for i in range(n):
             product = 1
-            for j in range(n): 
+            for j in range(n):
                 if i == j:
-                    continue                    
+                    continue
                 product *= nums[j]
             products.append(product)
 
         return products
 
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-            result = [1] * len(nums)
+    def product_except_self(self, nums: List[int]) -> List[int]:
+        """Compute product of array except self with O(n) time complexity."""
+        result = [1] * len(nums)
 
-            prefix = 1
-            for i in range(len(nums)): 
-                result[i] = prefix
-                prefix *= nums[i]
+        prefix = 1
+        for i, num in enumerate(nums):
+            result[i] = prefix
+            prefix *= num
 
-            postfix = 1
-            for i in range(len(nums)-1, -1, -1): 
-                result[i] *= postfix
-                postfix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
 
-            return result
+        return result
